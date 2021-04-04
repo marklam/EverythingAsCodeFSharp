8f32bcfb3ff932c53d7d7540152b687399d38bdc
Mark Lambert <marklam@spunow.co.uk>
2021-04-04 20:44:49 +0100

    Getting started doc and Repo example

---
+++ MkRepo/Program.fs
@@ -0,0 +1,25 @@
+﻿module Program
+
+open Pulumi.FSharp
+open Pulumi.Github
+
+let infra () =
+    let repo =
+        Repository(
+            "EverythingAsCodeFSharp",
+            RepositoryArgs(
+                Name = input "EverythingAsCodeFSharp",
+                Description = input "Generated from MkRepo",
+                Visibility = input "private",
+                GitignoreTemplate = input "VisualStudio"
+            )
+        )
+
+    // Export outputs here
+    dict [ 
+        ("EverythingAsCodeFSharp.Clone", repo.HttpCloneUrl :> obj) 
+    ]
+
+[<EntryPoint>]
+let main _ =
+  Deployment.run infra
