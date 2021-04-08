a8fc54817f5b7c162e39f77a41a6f06c064e04f5
Mark Lambert <mark.lambert@biosignatures.com>
2021-04-08 21:46:22 +0100

    Test that the function behaves as expected under func.exe

---
+++ 03 Automated Tests.md
@@ -0,0 +1,25 @@
+## Add an automated test
+I'd already begun moving on to the next stages when I realised that I was failing at one of my objectives. 
+I had tested the Azure Function locally with the `func` command, and a web browser - but of course I wouldn't remember to do that every time I made a change. I need to have checks like that done automatically (or at least automated).
+
+So, time to add a test project.
+```cmd
+mkdir WordValues.Azure.Tests
+cd WordValues.Azure.Tests
+dotnet new xunit --language F#
+```
+That produces an F# project using [xUnit](https://xunit.net/) as the test framework.
+
+### Testing under `func.exe`
+Http-triggered functions can be tested by connecting to the endpoint that is created when `func start` is used in the Azure Function project's source folder.
+
+To do this, we can use xUnit's [class fixtures](https://xunit.net/docs/shared-context#class-fixture) 
+- Run `func start` in the class fixture with a known port.
+- When a test needs to access the function, check the `func.exe` process has not exited, and try to connect to that port. If all is ok, return some connection context.
+- When the fixture is disposed, kill the process we started.
+### Testing the Http-Triggered function
+In the test case, we can use the class fixture to get the connection (which will check that the hosting process hasn't gone away)
+
+[FsHttp](https://github.com/ronaldschlenker/FsHttp) provides helpers to make the request/response code simple, and then the status code and response body can be checked in the test.
+
+It's a slow test to run compared to real unit-tests, but it's faster than running func manually and pasting urls into the web browser, and it can be done automatically by just running `dotnet test`
---
+++ WordValues.Azure.Tests/AzureFunc.fs
@@ -0,0 +1,73 @@
+﻿namespace WordValues.Azure.Tests
+
+open System
+open System.Diagnostics
+open System.Threading
+open System.Net.Http
+open System.Net.Sockets
+
+open Xunit
+
+module Assert =
+    let Fail message =
+        Assert.True(false, message)
+        Unchecked.defaultof<_> // Unreachable code, here to make conditional branches have matching return types
+
+type AzureFuncConnection =
+    { Client : HttpClient; BaseUri : Uri }
+    interface IDisposable with
+        member this.Dispose() = this.Client.Dispose()
+
+type AzureFuncInstance (folder, port) =
+    let timeoutSeconds = 30
+
+    let waitForPort (proc : Process) =
+        let sw = Stopwatch()
+        sw.Start()
+        let mutable portFound = false
+        while (sw.ElapsedMilliseconds < int64 (timeoutSeconds * 1000)) && (not portFound) && (not proc.HasExited) do
+            Thread.Sleep(TimeSpan.FromSeconds 1.)
+            try
+                use c = new TcpClient("127.0.0.1", port)
+                portFound <- true
+            with | :? SocketException -> ()
+
+        if proc.HasExited then
+            Error $"func.exe has exited with error code %d{proc.ExitCode}"
+        elif portFound then
+            Ok {
+                Client = new HttpClient(Timeout = TimeSpan.FromSeconds (float timeoutSeconds))
+                BaseUri = Uri($"http://localhost:%d{port}", UriKind.Absolute)
+            }
+        else
+            Error $"func.exe did not open port %d{port} within %d{timeoutSeconds} seconds"
+
+    // TODO - Capture stdout/stderr for diagnostic
+    let startInfo =
+        ProcessStartInfo(
+            FileName = "func.exe",
+            WorkingDirectory = folder,
+            UseShellExecute = false,
+            Arguments = $"start --port %d{port} --timeout %d{timeoutSeconds}")
+
+    let proc = Process.Start(startInfo)
+
+    let connection = lazy (waitForPort proc)
+
+    interface IDisposable with
+        member _.Dispose() =
+            try
+                proc.Kill(entireProcessTree=true)
+            with
+            | :? InvalidOperationException -> ()
+
+    member _.GetConnection() =
+        match connection.Value with
+        | Ok c ->
+            if proc.HasExited
+            then Assert.Fail $"func.exe has exited with error code %d{proc.ExitCode}"
+            else c
+        | Error msg ->
+            Assert.Fail msg
+
+
