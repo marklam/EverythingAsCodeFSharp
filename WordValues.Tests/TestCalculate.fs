f1e9ac07aabf3f1a55ffcea22cb5f24fe03f8f4f
Mark Lambert <marklam@spunow.co.uk>
2021-04-18 20:03:15 +0100

    Calculate a word value, add some test

---
+++ WordValues.Tests/TestCalculate.fs
@@ -0,0 +1,14 @@
+﻿module WordValues.Tests.TestCalculate
+
+open Xunit
+open Swensen.Unquote
+
+open WordValues
+
+[<Fact>]
+let ``Value of 'HELLO' is correct`` () =
+      test <@ Calculate.wordValue "HELLO" = 8 + 5 + 12 + 12 + 15 @>
+
+[<Fact>]
+let ``Value of 'hello' is correct`` () =
+      test <@ Calculate.wordValue "hello" = 8 + 5 + 12 + 12 + 15 @>
