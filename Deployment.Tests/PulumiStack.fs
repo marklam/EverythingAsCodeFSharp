1b3a7abb7faceb01b5668e166dd1d95ea2c09a6a
Mark Lambert <marklam@spunow.co.uk>
2021-04-18 10:45:58 +0100

    Test the deployed function by grabbing endpoint from Pulumi deployment's outputs

---
+++ Deployment.Tests/PulumiStack.fs
@@ -0,0 +1,18 @@
+﻿namespace Deployment.Tests
+
+open FSharp.Control.Tasks
+
+open Pulumi.Automation
+
+type PulumiStack (stackName, folder) =
+    let outputs =
+        task {
+            let  args    = LocalProgramArgs(stackName, folder)
+            let! stack   = LocalWorkspace.SelectStackAsync(args)
+            let! outputs = stack.GetOutputsAsync()
+            return outputs
+        }
+
+    member _.GetOutputs() =
+        outputs.Result
+
---
+++ Deployment.Tests/Tests.fs
@@ -0,0 +1,34 @@
+﻿namespace Deployment.Tests.Azure
+
+open System
+open System.IO
+open System.Net
+
+open FsHttp
+open FsHttp.Dsl
+
+open Xunit
+
+open Deployment.Tests
+
+module Deployment =
+    let folder = Path.Combine(DirectoryInfo(__SOURCE_DIRECTORY__).Parent.FullName, "Deployment")
+
+type AzurePulumiStackInstance() =
+    inherit PulumiStack("dev", Deployment.folder)
+
+// TODO - category for slow tests that require cloud function
+type TestAzureFun (stack : AzurePulumiStackInstance) =
+    interface IClassFixture<AzurePulumiStackInstance>
+
+    [<Fact>]
+    member _.``WordValue returns dummy Hello message`` () =
+        let outputs = stack.GetOutputs()
+        let testUri = Uri(outputs.["endpoint"].Value :?> string, UriKind.Absolute)
+
+        let response =
+            get testUri.AbsoluteUri
+            |> Request.send
+
+        Assert.Equal(HttpStatusCode.OK, response.statusCode)
+        Assert.Equal("Hello", response |> Response.toText)
