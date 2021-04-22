fb5915246ab3cfb5ba80114684c510e6c9b7c173
Mark Lambert <mark.lambert@biosignatures.com>
2021-04-22 17:28:37 +0100

    Make the Azure function return calculated values and warnings.

--- WordValues.Azure.Tests/Tests.fs
+++ WordValues.Azure.Tests/Tests.fs
@@ -1,7 +1,7 @@
 namespace WordValues.Azure.Tests
 
 open System
-open System.IO
+open System.Text.Json
 open System.Net
 
 open FsHttp
@@ -9,18 +9,15 @@ open FsHttp.Dsl
 
 open Xunit
 
-module WordValuesAzureFunc =
-    let folder = Path.Combine(DirectoryInfo(__SOURCE_DIRECTORY__).Parent.FullName, "WordValues.Azure")
-
 type WordValuesAzureFuncInstance() =
-    inherit AzureFuncInstance(WordValuesAzureFunc.folder, 7071)
+    inherit AzureFuncInstance(typeof<WordValuesAzureFuncInstance>, <@ WordValues.Azure.Program.main @>, 7071)
 
 // TODO - category for slow tests that require func.exe
 type TestAzureFun (func : WordValuesAzureFuncInstance) =
     interface IClassFixture<WordValuesAzureFuncInstance>
 
     [<Fact>]
-    member _.``WordValue returns dummy Hello message`` () =
+    member _.``WordValue with no query parameter returns an error`` () =
         use connection = func.GetConnection()
         let testUri = Uri(connection.BaseUri, "/api/WordValue")
 
@@ -28,5 +25,32 @@ type TestAzureFun (func : WordValuesAzureFuncInstance) =
             get testUri.AbsoluteUri
             |> Request.send
 
+        Assert.Equal(HttpStatusCode.BadRequest, response.statusCode)
+        Assert.Equal("Required query parameter 'word' was missing", response |> Response.toText)
+
+    [<Fact>]
+    member _.``WordValue returns the correct message`` () =
+        use connection = func.GetConnection()
+        let testUri = Uri(connection.BaseUri, "/api/WordValue?word=Hello")
+
+        let response =
+            get testUri.AbsoluteUri
+            |> Request.send
+
         Assert.Equal(HttpStatusCode.OK, response.statusCode)
-        Assert.Equal("Hello", response |> Response.toText)
+        Assert.Equal("""{"Value":52}""", response |> Response.toText)
+
+    [<Fact>]
+    member _.``WordValue returns warnings for non-letters`` () =
+        use connection = func.GetConnection()
+        let testUri = Uri(connection.BaseUri, "/api/WordValue?word=" + Uri.encodeUrlParam "Hello 123")
+
+        let response =
+            get testUri.AbsoluteUri
+            |> Request.send
+
+        Assert.Equal(HttpStatusCode.OK, response.statusCode)
+
+        let result = response |> Response.toText |> JsonDocument.Parse
+        Assert.Equal(52, result.RootElement.GetProperty("Value").GetInt32())
+        Assert.Equal("Ignored ' ','1','2','3'", result.RootElement.GetProperty("Warning").GetProperty("Value").GetString())
