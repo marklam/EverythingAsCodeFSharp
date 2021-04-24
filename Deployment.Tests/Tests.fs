649ecd240560e7c469c0e3e854a2e2da5f4a119c
Mark Lambert <marklam@spunow.co.uk>
2021-04-24 16:30:06 +0100

    Perform same tests on deployed function as locally hosted

--- Deployment.Tests/Tests.fs
+++ Deployment.Tests/Tests.fs
@@ -3,6 +3,7 @@
 open System
 open System.IO
 open System.Net
+open System.Text.Json
 
 open FsHttp
 open FsHttp.Dsl
@@ -23,7 +24,7 @@ type TestAzureFun (stack : AzurePulumiStackInstance) =
     interface IClassFixture<AzurePulumiStackInstance>
 
     [<Fact>]
-    member _.``WordValue returns dummy Hello message`` () =
+    member _.``WordValue with no query parameter returns an error`` () =
         let outputs = stack.GetOutputs()
         let testUri = Uri(outputs.["endpoint"].Value :?> string, UriKind.Absolute)
 
@@ -31,5 +32,34 @@ type TestAzureFun (stack : AzurePulumiStackInstance) =
             get testUri.AbsoluteUri
             |> Request.send
 
+        Assert.Equal(HttpStatusCode.BadRequest, response.statusCode)
+        Assert.Equal("Required query parameter 'word' was missing", response |> Response.toText)
+
+    [<Fact>]
+    member _.``WordValue returns the correct message`` () =
+        let outputs = stack.GetOutputs()
+        let baseUri = Uri(outputs.["endpoint"].Value :?> string, UriKind.Absolute)
+        let testUri = Uri(baseUri, "/api/WordValue?word=Hello")
+
+        let response =
+            get testUri.AbsoluteUri
+            |> Request.send
+
         Assert.Equal(HttpStatusCode.OK, response.statusCode)
-        Assert.Equal("Hello", response |> Response.toText)
+        Assert.Equal("""{"Value":52}""", response |> Response.toText)
+
+    [<Fact>]
+    member _.``WordValue returns warnings for non-letters`` () =
+        let outputs = stack.GetOutputs()
+        let baseUri = Uri(outputs.["endpoint"].Value :?> string, UriKind.Absolute)
+        let testUri = Uri(baseUri, "/api/WordValue?word=" + Uri.encodeUrlParam "Hello 123")
+
+        let response =
+            get testUri.AbsoluteUri
+            |> Request.send
+
+        Assert.Equal(HttpStatusCode.OK, response.statusCode)
+
+        let result = response |> Response.toText |> JsonDocument.Parse
+        Assert.Equal(52, result.RootElement.GetProperty("Value").GetInt32())
+        Assert.Equal("Ignored ' ','1','2','3'", result.RootElement.GetProperty("Warning").GetProperty("Value").GetString())
