6c7d44621d2e8b7bae23bfbe19d96521d03e4ff9
Mark Lambert <marklam@spunow.co.uk>
2021-04-25 21:33:13 +0100

    Fix compilation warnings

--- WordValues.Aws/Function.fs
+++ WordValues.Aws/Function.fs
@@ -6,7 +6,7 @@ open Amazon.Lambda.Serialization.SystemTextJson
 
 open System
 
-// This project specifies the serializer used to convert Lambda event into .NET classes in the project's main 
+// This project specifies the serializer used to convert Lambda event into .NET classes in the project's main
 // main function. This assembly register a serializer for use when the project is being debugged using the
 // AWS .NET Mock Lambda Test Tool.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
@@ -18,14 +18,14 @@ module Function =
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     ///
-    /// To use this handler to respond to an AWS event, reference the appropriate package from 
+    /// To use this handler to respond to an AWS event, reference the appropriate package from
     /// https://github.com/aws/aws-lambda-dotnet#events
     /// and change the string input parameter to the desired event type.
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    let functionHandler (input: string) (_: ILambdaContext) =
+    let functionHandler (input: string) (context: ILambdaContext) =
         match input with
         | null -> String.Empty
         | _ -> input.ToUpper()
@@ -33,10 +33,10 @@ module Function =
 
     [<EntryPoint>]
     let main _args =
-    
+
         let handler = Func<string, ILambdaContext, string>(functionHandler)
         use handlerWrapper = HandlerWrapper.GetHandlerWrapper(handler, new DefaultLambdaJsonSerializer())
         use bootstrap = new LambdaBootstrap(handlerWrapper)
-   
+
         bootstrap.RunAsync().GetAwaiter().GetResult()
         0
