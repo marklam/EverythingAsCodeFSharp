b79e15a6c79af79a3c67c00fb56dc3a6288c7b7d
Mark Lambert <marklam@spunow.co.uk>
2021-05-27 20:20:01 +0100

    Remove duplication from the Aws deployment

--- 17 Second Tidy-up.md
+++ 17 Second Tidy-up.md
@@ -1,5 +1,35 @@
 ## Second tidy-up
-- Some of the Deployment stuff was named "Deployment" or "Deployment.Aws", so I renamed "Deployment" to "Deployment.Azure".
-- Moved the PulumiExtras.fs file into its own library used from the Deployment.* projects, I used `dotnet new classlib --language F# --framework netcoreapp3.1` because the Pulumi libraries target netcoreapp3.1 - then renamed it as PulumiExtras.Core so it didn't start `Pulumi.` like the official libs
-- Tidied up the Deployment.Tests project to remove duplication, moved the tests into an abstract base class which takes an endpoint getter (abstract so the runner doesn't try to instanatiate the base class)
+### Make Azure projects explicit
+Some of the Deployment stuff was named "Deployment" or "Deployment.Aws", so I renamed "Deployment" to "Deployment.Azure".
+### Remove duplicated Pulumi helpers from Deployment projects
+Moved the PulumiExtras.fs file into its own library used from the Deployment.* projects, I used;
+```cmd
+dotnet new classlib --language F# --framework netcoreapp3.1`
+```
+because the Pulumi libraries target netcoreapp3.1
 
+Later I renamed the project as PulumiExtras.Core so it didn't start `Pulumi.` like an official lib would.
+### Remove duplication from all the Deployment.Tests fixtures
+I moved the tests into an abstract base class which takes an endpoint getter. The base class is abstract so that the runner doesn't try to instanatiate it as a separate fixture.
+### Reduce duplication in the Deployment.Aws project
+There was lots of duplication for the .net lambda vs the JS lambda, mostly because of all the setup around the Gateway. Unfortunately, each component needs references to multiple previous components.
+
+I found a solution that kept me fairly happy (at least for now) by:
+- adding functions to build each component
+- each function takes an anonymous record for context
+- the function returns the context with a new field added, eg 
+```fsharp
+let anonymousAnyMethod name (ctx : {| Resource : Resource; RestApi : RestApi |}) =
+        let method =
+            ApiGateway.Method(
+                name,
+                ApiGateway.MethodArgs(
+                    HttpMethod    = input "ANY",
+                    Authorization = input "NONE",
+                    RestApi       = io ctx.RestApi.Id,
+                    ResourceId    = io ctx.Resource.Id
+                )
+            )
+        {| ctx with Method = method |}
+```
+- these functions can then be piped.
--- Deployment.Aws/Pulumi.Extras.Aws.fs
+++ Deployment.Aws/Pulumi.Extras.Aws.fs
@@ -24,12 +24,12 @@ module File =
 
 [<RequireQualifiedAccess>]
 module S3 =
-    let uploadCode (bucket : S3.Bucket) blobName zipFilePath =
+    let uploadCode name (bucket : S3.Bucket) blobName zipFilePath =
         let hash = File.base64SHA256 zipFilePath
 
         let blob =
             S3.BucketObject(
-                "lambdaCode",
+                name,
                 S3.BucketObjectArgs(
                     Bucket = io bucket.BucketName,
                     Key    = input blobName,
@@ -38,3 +38,135 @@ module S3 =
         )
 
         {| Hash = hash; Blob = blob |}
+
+module ApiGateway =
+    open Pulumi.Aws.ApiGateway
+
+    let defaultRestApiPolicy =
+        """{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+        "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+    },
+    {
+        "Action": "execute-api:Invoke",
+        "Resource": "*",
+        "Principal": "*",
+        "Effect": "Allow",
+        "Sid": ""
+    }
+    ]
+}"""
+
+    let proxyResource name (restApi : RestApi) =
+        let resource =
+            ApiGateway.Resource(
+                name,
+                ApiGateway.ResourceArgs(
+                    RestApi  = io restApi.Id,
+                    PathPart = input "{proxy+}",
+                    ParentId = io restApi.RootResourceId
+                )
+            )
+        {| Resource = resource; RestApi = restApi |}
+
+    let anonymousAnyMethod name (ctx : {| Resource : Resource; RestApi : RestApi |}) =
+        let method =
+            ApiGateway.Method(
+                name,
+                ApiGateway.MethodArgs(
+                    HttpMethod    = input "ANY",
+                    Authorization = input "NONE",
+                    RestApi       = io ctx.RestApi.Id,
+                    ResourceId    = io ctx.Resource.Id
+                )
+            )
+        {| ctx with Method = method |}
+
+    let awsProxyIntegration name (lambda : Lambda.Function) (ctx : {| Resource : Resource; RestApi : RestApi; Method : Method |}) =
+        let integration =
+            ApiGateway.Integration(
+                name,
+                ApiGateway.IntegrationArgs(
+                    HttpMethod            = input "ANY",
+                    IntegrationHttpMethod = input "POST",
+                    ResourceId            = io ctx.Resource.Id,
+                    RestApi               = io ctx.RestApi.Id,
+                    Type                  = input "AWS_PROXY",
+                    Uri                   = io lambda.InvokeArn
+                ),
+                CustomResourceOptions(
+                    DependsOn = InputList.ofSeq [ ctx.Method ]
+                )
+            )
+        {| ctx with Integration = integration |}
+
+    let deployment name description (ctx : {| Integration : Integration; Resource : Resource; RestApi : RestApi; Method : Method |}) =
+        let deployment =
+            ApiGateway.Deployment(
+                name,
+                ApiGateway.DeploymentArgs(
+                    Description      = input description,
+                    RestApi          = io ctx.RestApi.Id
+                ),
+                CustomResourceOptions(
+                    DependsOn = InputList.ofSeq [ ctx.Resource; ctx.Method; ctx.Integration ]
+                )
+            )
+        {| RestApi = ctx.RestApi; Deployment = deployment |}
+
+    let stage name (ctx : {| Deployment : Deployment; RestApi : RestApi |}) =
+        let stage =
+            ApiGateway.Stage(
+                name,
+                ApiGateway.StageArgs(
+                    Deployment = io ctx.Deployment.Id,
+                    RestApi    = io ctx.RestApi.Id,
+                    StageName  = input "dev"
+                )
+            )
+        {| Stage = stage; Deployment = ctx.Deployment |}
+
+module Lambda =
+    let apiPermission name region accountId (restApi : ApiGateway.RestApi) (lambda : Lambda.Function) =
+        let executionArn =
+            (accountId, restApi.Id)
+            ||> Output.map2 (fun accId gwId -> $"arn:aws:execute-api:%s{region}:%s{accId}:%s{gwId}/*/*/*")
+
+        Lambda.Permission(
+            name,
+            Lambda.PermissionArgs(
+                Action            = input "lambda:InvokeFunction",
+                Function          = io lambda.Name,
+                Principal         = input "apigateway.amazonaws.com",
+                SourceArn         = io executionArn,
+                StatementIdPrefix = input "lambdaPermission"
+            ),
+            CustomResourceOptions(
+                DeleteBeforeReplace = true
+            )
+        )
+
+    let proxyPermission name (ctx : {| Stage : ApiGateway.Stage; Deployment : ApiGateway.Deployment; Lambda : Lambda.Function |}) =
+        let proxyArn =
+            (ctx.Deployment.ExecutionArn, ctx.Stage.StageName)
+            ||> Output.map2 (fun execArn stageName -> $"{execArn}{stageName}/*/{{proxy+}}")
+
+        let lambdaProxyPermission =
+            Lambda.Permission(
+                name,
+                Lambda.PermissionArgs(
+                    Action    = input "lambda:InvokeFunction",
+                    Function  = io ctx.Lambda.Arn,
+                    Principal = input "apigateway.amazonaws.com",
+                    SourceArn = io proxyArn
+                )
+            )
+
+        lambdaProxyPermission
