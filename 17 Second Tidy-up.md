291e1b46cac000690249f59dd0f5a1dc3f3945aa
Mark Lambert <marklam@spunow.co.uk>
2021-05-27 13:49:45 +0100

    Move the deployed lambda/function tests into a base class to use the same tests on all versions

--- 17 Second Tidy-up.md
+++ 17 Second Tidy-up.md
@@ -1,4 +1,5 @@
 ## Second tidy-up
 - Some of the Deployment stuff was named "Deployment" or "Deployment.Aws", so I renamed "Deployment" to "Deployment.Azure".
 - Moved the PulumiExtras.fs file into its own library used from the Deployment.* projects, I used `dotnet new classlib --language F# --framework netcoreapp3.1` because the Pulumi libraries target netcoreapp3.1 - then renamed it as PulumiExtras.Core so it didn't start `Pulumi.` like the official libs
+- Tidied up the Deployment.Tests project to remove duplication, moved the tests into an abstract base class which takes an endpoint getter (abstract so the runner doesn't try to instanatiate the base class)
 
--- Deployment.Tests/AzureJS.fs
+++ Deployment.Tests/AzureJS.fs
@@ -1,57 +1,12 @@
-﻿namespace Deployment.Tests.AzureJS
+﻿namespace Deployment.Tests.Azure
 
 open System
-open System.Net
-open System.Text.Json
-
-open FsHttp
-open FsHttp.Dsl
 
 open Xunit
 
-open Deployment.Tests.Azure
+open Deployment.Tests
 
 // TODO - category for slow tests that require cloud function
-type TestAzureJSFun (stack : AzurePulumiStackInstance) =
+type TestAzureJSFunc (stack : AzurePulumiStackInstance) =
+    inherit TestWordValueEndpoints(fun () -> Uri(stack.GetOutputs().["jsEndpoint"].Value :?> string, UriKind.Absolute))
     interface IClassFixture<AzurePulumiStackInstance>
-
-    [<Fact>]
-    member _.``WordValue with no query parameter returns an error`` () =
-        let outputs = stack.GetOutputs()
-        let testUri = Uri(outputs.["jsEndpoint"].Value :?> string, UriKind.Absolute)
-
-        let response =
-            get testUri.AbsoluteUri
-            |> Request.send
-
-        Assert.Equal(HttpStatusCode.BadRequest, response.statusCode)
-        Assert.Equal("Required query parameter 'word' was missing", response |> Response.toText)
-
-    [<Fact>]
-    member _.``WordValue returns the correct message`` () =
-        let outputs = stack.GetOutputs()
-        let baseUri = Uri(outputs.["jsEndpoint"].Value :?> string, UriKind.Absolute)
-        let testUri = Uri(baseUri, "/api/WordValue?word=Hello")
-
-        let response =
-            get testUri.AbsoluteUri
-            |> Request.send
-
-        Assert.Equal(HttpStatusCode.OK, response.statusCode)
-        Assert.Equal("""{"Value":52}""", response |> Response.toText)
-
-    [<Fact>]
-    member _.``WordValue returns warnings for non-letters`` () =
-        let outputs = stack.GetOutputs()
-        let baseUri = Uri(outputs.["jsEndpoint"].Value :?> string, UriKind.Absolute)
-        let testUri = Uri(baseUri, "/api/WordValue?word=" + Uri.encodeUrlParam "Hello 123")
-
-        let response =
-            get testUri.AbsoluteUri
-            |> Request.send
-
-        Assert.Equal(HttpStatusCode.OK, response.statusCode)
-
-        let result = response |> Response.toText |> JsonDocument.Parse
-        Assert.Equal(52, result.RootElement.GetProperty("Value").GetInt32())
-        Assert.Equal("Ignored ' ','1','2','3'", result.RootElement.GetProperty("Warning").GetString())
