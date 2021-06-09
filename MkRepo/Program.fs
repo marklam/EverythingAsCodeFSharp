271aebfec4c0539719c697b27050f4c984baa670
Mark Lambert <marklam@spunow.co.uk>
2021-06-09 13:50:15 +0100

    Make the EverythinAsCodeFSharp repo public

--- MkRepo/Program.fs
+++ MkRepo/Program.fs
@@ -9,15 +9,15 @@ let infra () =
             "EverythingAsCodeFSharp",
             RepositoryArgs(
                 Name = input "EverythingAsCodeFSharp",
-                Description = input "Generated from MkRepo",
-                Visibility = input "private",
+                Description = input "Cloud projects with devops, deployment and build all done in code. In F#.",
+                Visibility = input "public",
                 GitignoreTemplate = input "VisualStudio"
             )
         )
 
     // Export outputs here
-    dict [ 
-        ("EverythingAsCodeFSharp.Clone", repo.HttpCloneUrl :> obj) 
+    dict [
+        ("EverythingAsCodeFSharp.Clone", repo.HttpCloneUrl :> obj)
     ]
 
 [<EntryPoint>]
