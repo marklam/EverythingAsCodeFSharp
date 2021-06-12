8291357b374a5bd6a8a764fe9a39b241f3930e63
Mark Lambert <marklam@spunow.co.uk>
2021-06-12 10:35:54 +0100

    Create a Services project for services needed by all the functions/lambdas, eg Logging

---
+++ Services/Logging.fs
@@ -0,0 +1,22 @@
+﻿namespace Services
+
+open System
+
+type LogLevel =
+    | Trace
+    | Debug
+    | Info
+    | Warn
+    | Error
+    | Critical
+
+[<Struct>]
+type LogEvent = { Message : string; EventId : int; Params : obj[] } with
+    static member Create(message, [<ParamArray>] pars) = { Message = message; EventId = 0; Params = pars }
+
+type ILogger =
+    abstract Log : LogLevel -> LogEvent -> unit
+
+module Log =
+    let info (logger : ILogger) event =
+        logger.Log Info event
